8f39a5937ee55dd5127b412986a2df5c74f22b51
Maxim <the_prince@sibmail.com>
2021-02-06 02:37:27 +0700

    Задание №1

--- Java-1/src/Lesson_2.java
+++ Java-1/src/Lesson_2.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Lesson_2 {
+public class Lesson_2 {
+    public static void main(String[] args) {
+
+        doTask1();
+    }
+
+    /**
+     * 1.  Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+    static void doTask1() {
+        int[] mass = {1, 1, 0, 0, 1, 1, 0, 0};
+        System.out.print("Заданный массив m = ");
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i]);
+        }
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] == 1) {
+                mass[i] = 0;
+            } else {
+                mass[i] = 1;
+            }
+        }
+        System.out.println();
+        System.out.print("Результат m = ");
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i]);
+        }
+    }
+
 }
