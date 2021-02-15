a866ef10ca194a5ea54c5450860097f71242f4fe
Maxim <the_prince@sibmail.com>
2021-02-15 07:31:03 +0700

    1. Создание класса "Сотрудники"с полями. ФИО, должность, email, телефон, зарплата, возраст.

---
+++ Java-1/src/Lesson_5/Sotrudnik.java
@@ -0,0 +1,12 @@
+package Lesson_5;
+
+public class Sotrudnik {
+    private String family;
+    private String name;
+    private String otchestvo;
+    private String profi;
+    private String email;
+    private int number;
+    private int zarplatra;
+    private int age;
+}
