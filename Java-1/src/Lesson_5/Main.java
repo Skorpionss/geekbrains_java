cf10eefcbc4a1d590ca107fa7112083b9e2685d5
Maxim <the_prince@sibmail.com>
2021-02-15 11:53:55 +0700

    4. Создание массива из 5 сотрудников

--- Java-1/src/Lesson_5/Main.java
+++ Java-1/src/Lesson_5/Main.java
@@ -1,4 +1,26 @@
 package Lesson_5;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void masSotrudnik() {
+        Sotrudnik[] sotrudnik = {
+                new Sotrudnik("Николаев", "Александр", "Владимирович",
+                        "гл. Инжинер", "vlad_dd@mail.ru", 5505050, 40000, 35),
+                new Sotrudnik("Андреев", "Владимир", "Олегович",
+                        "энергетик", "andreii@mail.ru", 6008080, 35000, 30),
+                new Sotrudnik("Алексеев", "Максим", "Николаевич",
+                        "гл. программист", "maxaleks@mail.ru", 7007777, 500000, 42),
+                new Sotrudnik("Иванов", "Андрей", "Владимирович",
+                        "слесарь", "ivanow41@mail.ru", 8909090, 37000, 41),
+                new Sotrudnik("Игнатенко", "Алексей", "Максимович",
+                        "сантехник", "waleks33@mail.ru", 2001111, 30000, 38)
+        };
+
+    }
+
+
 }
