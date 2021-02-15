66d6133439d4320f0667a0300a72af99eb8140ae
Maxim <the_prince@sibmail.com>
2021-02-15 09:07:12 +0700

    2. Создание полного конструктора.

--- Java-1/src/Lesson_5/Sotrudnik.java
+++ Java-1/src/Lesson_5/Sotrudnik.java
@@ -9,4 +9,15 @@ public class Sotrudnik {
     private int number;
     private int zarplatra;
     private int age;
+
+    public Sotrudnik(String family, String name, String otchestvo, String profi, String email, int number, int zarplatra, int age) {
+        this.family = family;
+        this.name = name;
+        this.otchestvo = otchestvo;
+        this.profi = profi;
+        this.email = email;
+        this.number = number;
+        this.zarplatra = zarplatra;
+        this.age = age;
+    }
 }
