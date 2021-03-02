a4e270b0028c732a7976715ea881da11786ff6d1
Maxim <the_prince@sibmail.com>
2021-03-03 04:05:28 +0700

    1 - Реализация умножение, сложение, вычитание, деление

--- Java-1/src/Lesson_8/BottomFrame.java
+++ Java-1/src/Lesson_8/BottomFrame.java
@@ -7,10 +7,13 @@ import java.awt.event.ActionListener;
 
 public class BottomFrame {
     private final JPanel panel;
+    private int left;
+    private int right;
+    private String operacia;
 
     public BottomFrame(JTextField inputField) {
         panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 3));
+        panel.setLayout(new GridLayout(6, 3));
 
         DigitButtonListener buttonListener = new DigitButtonListener(inputField);
 
@@ -20,18 +23,86 @@ public class BottomFrame {
             panel.add(btn);
         }
 
-        panel.add(new JButton("="));
+        JButton ravno = new JButton("=");
+        ravno.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                right = Integer.parseInt(inputField.getText());
+                switch (operacia) {
+                    case ("+"):
+                        inputField.setText(String.valueOf(left + right));
+                        break;
+                    case ("-"):
+                        inputField.setText(String.valueOf(left - right));
+                        break;
+                    case ("/"):
+                        inputField.setText(String.valueOf(left / right));
+                        break;
+                    case ("*"):
+                        inputField.setText(String.valueOf(left * right));
+                        break;
+                }
+
+            }
+        });
+        panel.add(ravno);
+
+
         JButton clear = new JButton("C");
         clear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 inputField.setText("");
+                left = 0;
             }
         });
         panel.add(clear);
-        panel.add(new JButton("+"));
-        panel.add(new JButton("-"));
+
+        JButton plus = new JButton("+");
+        plus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                left = Integer.parseInt(inputField.getText());
+                inputField.setText("");
+                operacia = "+";
+            }
+        });
+        panel.add(plus);
+
+        JButton minus = new JButton("-");
+        minus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                left = Integer.parseInt(inputField.getText());
+                inputField.setText("");
+                operacia = "-";
+            }
+        });
+        panel.add(minus);
+
         panel.add(new JButton("SQRT"));
+
+        JButton delim = new JButton("/");
+        delim.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                left = Integer.parseInt(inputField.getText());
+                inputField.setText("");
+                operacia = "/";
+            }
+        });
+        panel.add(delim);
+
+        JButton umnoshit = new JButton("*");
+        umnoshit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                left = Integer.parseInt(inputField.getText());
+                inputField.setText("");
+                operacia = "*";
+            }
+        });
+        panel.add(umnoshit);
 
     }
 
